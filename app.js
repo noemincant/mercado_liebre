edd62c71826106f99a0fd8cfebc191b61307aa54
noemincant <noemincant@gmail.com>
2022-03-29 16:31:43 -0300

    ll

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+
+app.get("/home", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+app.listen(4001, () => console.log("running on port 4001..."));
